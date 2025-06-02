87f742f2633a45343192c0cdf2bb3173f6076962
Mykry <kryukovm1khael@yandex.ru>
2025-06-02 16:25:26 +0500

    docker fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import Register from './Register';
-import DashboardPage from './DashboardPage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import Register from './Register'
+import DashboardPage from './DashboardPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
-);
+)
