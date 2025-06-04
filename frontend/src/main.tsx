5f066db06030ce7e56d77fb30455a0476551cbf4
Mykry <kryukovm1khael@yandex.ru>
2025-06-04 22:26:42 +0500

    backend + frontend = fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,13 +9,11 @@ import { DashboardPage } from './pages/DashboardPage';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
-    </BrowserRouter>
     </BrowserRouter>
   </React.StrictMode>
 )
